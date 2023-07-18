2e1b45173648db4b756a8bb16cbe8534f251a0b1
Attagon47 <attagon.bo.65@ubu.ac.th>
2023-07-18 12:38:23 +0700

    react-create app

---
+++ src/contact.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Layout } from "./App";
+
+function Contact() {
+  return (
+    <>
+      <Layout />
+      <h3>Rk market</h3>
+      <div>
+        Welcome to Rk the keyboard
+        <br />
+        Is the best keyboard
+        <br />
+        Ubonrachatani
+        <br />
+        Thailand
+      </div>
+    </>
+  );
+}
+
+export default Contact;
